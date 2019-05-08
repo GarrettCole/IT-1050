aa88c22528b9e3db27d9164b57aa944c37c39337
GarrettCole <47068989+GarrettCole@users.noreply.github.com>
2019-05-07 23:23:11 -0400

    FinalExam

---
+++ FinalExam/ConsoleApp3/Passenger.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp3
+{
+    class Passenger
+    {
+        private string Name;
+        private double Weight;
+        public Passenger(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public double GetWeight()
+        {
+            return Weight;
+        }
+    
+    }
+}
